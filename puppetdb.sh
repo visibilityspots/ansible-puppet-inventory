5c53f7b2009d2f7cf91a14f762e629ac73ddc3b0
Jan Collijs <jan.collijs@inuits.eu>
2014-10-22 08:39:42 +0200

    Bugfix: using relative path for environments

--- puppetdb.sh
+++ puppetdb.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 #
 # Script which can be used as an ansible dynamic inventory using the puppetdb API
+#
+# Source: https://github.com/visibilityspots/ansible-puppet-inventory
 
 # DECLARE PARAMETERS
-PUPPETDBHOST='HOST:PORT'
+ROOT_DIR='/etc/ansible/'
+PUPPETDBHOST='PUPPETDB_HOST:PUPPETDB_PORT'
 HOSTS='\n'
-ENVIRONMENTS=`ls environments | wc -l`
+TOTAL_ENVIRONMENTS=`ls $ROOT_DIR/environments | wc -l`
+ENVIRONMENTS=$ROOT_DIR/environments/*
 
 # LOOP THROUGH QUERIES AND STORE THE FOUND NODES INTO THE $HOSTS VARIABLE
-for ENVIRONMENT in environments/*
+for ENVIRONMENT in $ENVIRONMENTS
 do
-	ENV=`echo $ENVIRONMENT | awk -F "/" '{print $2}'`
-	HOSTS="${HOSTS}\t\"$ENV\": ["
-	FIRST_HOST="true"
+        ENV=`echo $ENVIRONMENT | awk -F "/" '{print $NF}'`
+        HOSTS="${HOSTS}\t\"$ENV\": ["
+        FIRST_HOST="true"
 
-	while read hostname
-	do
-	  # this way i don't get a trailing comma
-	  if [ "${FIRST_HOST}" == "true" ]
-	  then
-	    HOSTS="${HOSTS} \"${hostname}\""
-	    unset FIRST_HOST
-	  else
-	    HOSTS="${HOSTS}, \"${hostname}\""
-	  fi
-	done < <(curl --silent -X GET http://$PUPPETDBHOST/v4/nodes --data-urlencode query@$ENVIRONMENT | grep certname | awk -F "\"" '{print $4}')
+        while read hostname
+        do
+          # this way i don't get a trailing comma
+          if [ "${FIRST_HOST}" == "true" ]
+          then
+            HOSTS="${HOSTS} \"${hostname}\""
+            unset FIRST_HOST
+          else
+            HOSTS="${HOSTS}, \"${hostname}\""
+          fi
+  done < <(curl --silent -X GET http://$PUPPETDBHOST/v4/nodes --data-urlencode query@$ENVIRONMENT | grep certname | grep '.*' | awk -F "\"" '{print $(NF-1)}')
 
         # FIX FOR THE PARSE ERROR WHEN A COMMA IS FOUND AFTER LAST LIST OF NODES
-	i=$(expr $i + 1)
-	if [ $i == $ENVIRONMENTS ]; then
-		HOSTS="${HOSTS} ]\n"
-	else
-		HOSTS="${HOSTS} ],\n"
-	fi
+        i=$(expr $i + 1)
+        if [ $i == $TOTAL_ENVIRONMENTS ]; then
+                HOSTS="${HOSTS} ]\n"
+        else
+                HOSTS="${HOSTS} ],\n"
+        fi
 done
 
 # RETURN THE HOSTS IN JSON OUTPUT
